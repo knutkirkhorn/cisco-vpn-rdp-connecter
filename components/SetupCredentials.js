4e996ed46e8f6ab90c940642093bce8d8c9178f4
Knut Kirkhorn <knutkirk@hotmail.com>
2022-10-19 22:21:41 +0200

    Set `onlyVpn` during setup

--- components/SetupCredentials.js
+++ components/SetupCredentials.js
@@ -1,6 +1,6 @@
 const importJsx = require('import-jsx');
 const {Text, Box, useStdin} = require('ink');
-const {useState} = require('react');
+const {useState, useEffect} = require('react');
 const React = require('react');
 const SelectInput = require('ink-select-input').default;
 const {getAllCiscoVpnGroups} = require('..');
@@ -14,18 +14,48 @@ const STEPS = {
     GROUP: 1,
     USERNAME: 2,
     PASSWORD: 3,
-    RDP_SERVER: 4
+    RDP_SERVER: 4,
+    ONLY_VPN: 5
 };
+const yesNoOptions = [
+    {
+        label: 'No',
+        value: 'no'
+    },
+    {
+        label: 'Yes',
+        value: 'yes'
+    }
+];
 
 const SetupCredentials = ({onComplete, defaultCredentials}) => {
     const [vpnServer, setVpnServer] = useState('');
-    const [group, setGroup] = useState('');
+    const [group, setGroup] = useState();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [rdpServer, setRdpServer] = useState('');
+    const [onlyVpn, setOnlyVpn] = useState();
     const [step, setStep] = useState(STEPS.VPN_SERVER);
     const [vpnGroups, setVpnGroups] = useState();
     const {setRawMode} = useStdin();
     const [isRetrievingVpnGroups, setIsRetrievingVpnGroups] = useState(false);
+
+    useEffect(() => {
+        if (onlyVpn === undefined) return;
+
+        onComplete({
+            vpn: {
+                server: vpnServer,
+                group: group.value,
+                username,
+                password
+            },
+            rdp: {
+                server: rdpServer
+            },
+            onlyVpn
+        });
+    }, [onlyVpn]);
 
     const goToNextStep = () => {
         setStep(step + 1);
@@ -70,28 +100,18 @@ const SetupCredentials = ({onComplete, defaultCredentials}) => {
         goToNextStep();
     };
 
-    const onSetupCompleted = inputRdpServer => {
+    const onRdpServerSet = inputRdpServer => {
+        setRdpServer(inputRdpServer);
         goToNextStep();
+    };
 
-        if (!onComplete) {
-            return;
-        }
-
-        onComplete({
-            vpn: {
-                server: vpnServer,
-                group: group.value,
-                username,
-                password
-            },
-            rdp: {
-                server: inputRdpServer
-            }
-        });
+    const handleOnlyVpnSet = inputOnlyVpn => {
+        setOnlyVpn(inputOnlyVpn.value === 'yes');
+        goToNextStep();
     };
 
     // If all steps are completed show "completed" message
-    if (step > STEPS.RDP_SERVER) {
+    if (step > STEPS.ONLY_VPN) {
         return <SuccessMessage message="Setup completed" />;
     }
 
@@ -143,8 +163,18 @@ const SetupCredentials = ({onComplete, defaultCredentials}) => {
                 <TextAndInputBox
                     text="RDP server"
                     defaultText={defaultCredentials.rdp.server}
-                    onSubmit={onSetupCompleted}
+                    onSubmit={onRdpServerSet}
                 />
+            )}
+            {step >= STEPS.ONLY_VPN && (
+                <Box>
+                    <Text>Only connect to VPN: </Text>
+                    <SelectInput
+                        items={yesNoOptions}
+                        initialIndex={0}
+                        onSelect={handleOnlyVpnSet}
+                    />
+                </Box>
             )}
         </>
     );
